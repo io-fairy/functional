f0c8230f1b9a1cac049741bf583fd7f8aba7a436
iofairy <iofairy@foxmail.com>
2021-08-06 07:15:06 +0800

    Modifying `G` class document comment

--- functional/src/main/java/com/iofairy/top/G.java
+++ functional/src/main/java/com/iofairy/top/G.java
@@ -21,8 +21,8 @@ import java.util.*;
 
 /**
  * Global Variables And Methods. <br>
- * 常用的变量与函数<br>
- * 采用简单类名 <b>G</b> 模拟类似Kotlin的 <b>Top-level function</b> （顶级函数、全局函数））
+ * 常用的变量与函数。<br>
+ * 采用简单类名 <b>G</b> 模拟类似Kotlin的 <b>Top-level function</b>（顶级函数、全局函数）
  *
  * @since 0.0.1
  */
